7710dd4331ba1e3a4e60b610ffe774e5c31036ba
Chandan Rajah <chandan.rajah@gmail.com>
2023-04-16 11:26:12 +0000

    removed unused folders

--- proto-compiler-plugin/build.sbt
+++
@@ -1,4 +0,0 @@
-libraryDependencies ++= Seq(
-  "com.google.protobuf" % "protobuf-java" % "3.4.0",
-  "com.github.os72" % "protoc-jar" % "3.4.0"
-)
--- proto-compiler-plugin/src/main/scala/com/trueaccord/scalapb/compiler/Version.scala
+++
@@ -1,7 +0,0 @@
-package com.trueaccord.scalapb.compiler
-
-object Version {
-  val scalapbVersion = "0.6.7-SNAPSHOT"
-  val protobufVersion = "3.4.0"
-  val grpcJavaVersion = "1.7.0"
-}
