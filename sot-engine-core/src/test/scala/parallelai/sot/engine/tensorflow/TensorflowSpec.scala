bc3cf32545b3cacd45c1b450206e2c8bf0f598e9
Chandan Rajah <chandan.rajah@gmail.com>
2023-05-11 12:42:28 +0000

    example of extended regression model

--- sot-engine-core/src/test/scala/parallelai/sot/engine/tensorflow/TensorflowSpec.scala
+++ sot-engine-core/src/test/scala/parallelai/sot/engine/tensorflow/TensorflowSpec.scala
@@ -6,6 +6,8 @@ import com.spotify.scio.ContextAndArgs
 import com.spotify.scio.testing.{DistCacheIO, PipelineSpec, TextIO}
 import org.tensorflow._
 import com.spotify.scio.tensorflow._
+import scala.collection.JavaConversions._
+import scala.collection.JavaConverters._
 
 private object TFJob {
   def main(argv: Array[String]): Unit = {
@@ -35,10 +37,15 @@ private object TFJob2Inputs {
 private object TFJobLinearModel {
   def main(argv: Array[String]): Unit = {
     val (sc, args) = ContextAndArgs(argv)
-    sc.parallelize(List(Array(1.0f, 2.0f), Array(2.0f, 3.0f)))
+    sc.parallelize(List(Array(Array(1.0f, 2.0f)), Array(Array(2.0f, 3.0f))))
       .predict(args("graphURI"), Seq("predict"))
       {e => Map("input" -> Tensor.create(e))}
-      {o => o.map{case (_, t) => t.floatValue()}.head}
+      {o => o.map{
+        case (_, t) =>
+          val v = Array.ofDim[Float](1,1)
+          t.copyTo(v)
+          v.apply(0).apply(0)
+      }.head}
       .saveAsTextFile(args("output"))
     sc.close()
   }
@@ -73,31 +80,31 @@ class TensorflowSpec extends PipelineSpec {
     new String(r.bytesValue()) should be (helloworld)
   }
 
-//  it should "allow to predict" in {
-//    val g = new Graph()
-//    val t3 = Tensor.create(3L)
-//    val input = g.opBuilder("Placeholder", "input").setAttr("dtype", t3.dataType).build.output(0)
-//    val c3 = g.opBuilder("Const", "c3")
-//      .setAttr("dtype", t3.dataType)
-//      .setAttr("value", t3).build.output(0)
-//    g.opBuilder("Mul", "multiply").addInput(c3).addInput(input).build()
-//
-//
-//  }
-//
-//  it should "allow to predict with 2 inputs" in {
-//    val g = new Graph()
-//    val input = g.opBuilder("Placeholder", "input")
-//      .setAttr("dtype", DataType.INT64).build.output(0)
-//    val input2 = g.opBuilder("Placeholder", "input2")
-//      .setAttr("dtype", DataType.INT64).build.output(0)
-//    g.opBuilder("Mul", "multiply").addInput(input2).addInput(input).build()
-//    JobTest[TFJob2Inputs.type]
-//      .distCache(DistCacheIO[Array[Byte]]("tf-graph.bin"), g.toGraphDef)
-//      .args("--graphURI=tf-graph.bin", "--output=output")
-//      .output(TextIO("output"))(_ should containInAnyOrder((1L to 10).map(_ * 3).map(_.toString)))
-//      .run()
-//  }
+  it should "allow to predict" in {
+    val g = new Graph()
+    val t3 = Tensor.create(3L)
+    val input = g.opBuilder("Placeholder", "input").setAttr("dtype", t3.dataType).build.output(0)
+    val c3 = g.opBuilder("Const", "c3")
+      .setAttr("dtype", t3.dataType)
+      .setAttr("value", t3).build.output(0)
+    g.opBuilder("Mul", "multiply").addInput(c3).addInput(input).build()
+
+
+  }
+
+  it should "allow to predict with 2 inputs" in {
+    val g = new Graph()
+    val input = g.opBuilder("Placeholder", "input")
+      .setAttr("dtype", DataType.INT64).build.output(0)
+    val input2 = g.opBuilder("Placeholder", "input2")
+      .setAttr("dtype", DataType.INT64).build.output(0)
+    g.opBuilder("Mul", "multiply").addInput(input2).addInput(input).build()
+    JobTest[TFJob2Inputs.type]
+      .distCache(DistCacheIO[Array[Byte]]("tf-graph.bin"), g.toGraphDef)
+      .args("--graphURI=tf-graph.bin", "--output=output")
+      .output(TextIO("output"))(_ should containInAnyOrder((1L to 10).map(_ * 3).map(_.toString)))
+      .run()
+  }
 
   it should "allow to predict a linear model" in {
     val g = new Graph()
@@ -105,26 +112,29 @@ class TensorflowSpec extends PipelineSpec {
     val input = g.opBuilder("Placeholder", "input")
       .setAttr("dtype", DataType.FLOAT).build.output(0)
 
-    val x: Array[Array[Float]] = Array(Array(2.0f), Array(5.0f))
-
     val w1 = Tensor.create(Array(Array(2.0f), Array(5.0f)))
     val weight = g.opBuilder("Const", "weight")
       .setAttr("dtype", w1.dataType())
       .setAttr("value", w1).build().output(0)
 
-    val b1 = Tensor.create(1.5f)
+    val b1 = Tensor.create(2.0f)
     val bias = g.opBuilder("Const", "bias")
       .setAttr("dtype", b1.dataType())
       .setAttr("value", b1).build().output(0)
 
-    val mul = g.opBuilder("Mul", "multiply").addInput(input).addInput(weight).build().output(0)
+    val mul = g.opBuilder("MatMul", "multiply")
+      .setAttr("T", DataType.FLOAT)
+      .addInput(input)
+      .addInput(weight)
+      .build()
+      .output(0)
 
     g.opBuilder("Add", "predict").addInput(mul).addInput(bias).build()
 
     JobTest[TFJobLinearModel.type]
       .distCache(DistCacheIO[Array[Byte]]("tf-graph.bin"), g.toGraphDef)
       .args("--graphURI=tf-graph.bin", "--output=output")
-      .output(TextIO("output"))(_ should containInAnyOrder(List(Array(1.0f, 2.0f), Array(2.0f, 3.0f)).map(a => (a(0) * 2.0f) + (a(1) * 5.0f) + 2.0f).map(_.toString)))
+      .output(TextIO("output"))(_ should containInAnyOrder(List(Array(1.0f, 2.0f), Array(2.0f, 3.0f)).map(a => (a.apply(0) * 2.0f) + (a.apply(1) * 5.0f) + 2.0f).map(_.toString)))
       .run()
 
   }
