250a5b423e850088ec3e94d6b34737bf7fdccb26
Chandan Rajah <chandan.rajah@gmail.com>
2023-07-04 11:53:47 +0000

    Utilise in tap with memory sequence for TDD

--- sot-executor/src/main/scala/parallelai/sot/executor/builder/SOTBuilder.scala
+++ sot-executor/src/main/scala/parallelai/sot/executor/builder/SOTBuilder.scala
@@ -64,41 +64,38 @@ import parallelai.sot.engine.io.datastore._
 //// SOT Nats
 import parallelai.sot.executor.SOTNats._
 
+/**
+  * To run this class with a default configuration of application.conf:
+  * <pre>
+  * sbt clean compile "sot-executor/runMain parallelai.sot.executor.builder.SOTBuilder --project=bi-crm-poc --runner=DataflowRunner --region=europe-west1 --zone=europe-west2-a --workerMachineType=n1-standard-1 --diskSizeGb=150 --maxNumWorkers=1 --waitToFinish=false"
+  * </pre>
+  *
+  * If there is no application.conf then compilation will fail, but you can supply your own conf as a Java option e.g. -Dconfig.resource=application-ps2ps-test.conf
+  * <pre>
+  * sbt -Dconfig.resource=application-ps2ps-test.conf clean compile "sot-executor/runMain parallelai.sot.executor.builder.SOTBuilder --project=bi-crm-poc --runner=DataflowRunner --region=europe-west1 --zone=europe-west2-a --workerMachineType=n1-standard-1 --diskSizeGb=150 --maxNumWorkers=1 --waitToFinish=false"
+  * </pre>
+  * NOTE That application configurations can also be set/overridden via system and environment properties.
+  */
+@SOTBuilder
 object SOTBuilder {
-
-  @AvroType.fromSchema("{\"type\":\"record\",\"name\":\"Message\",\"namespace\":\"parallelai.sot.avro\",\"fields\":[{\"name\":\"user\",\"type\":\"string\",\"doc\":\"Name of the user\"},{\"name\":\"teamName\",\"type\":\"string\",\"doc\":\"Name of the team\"},{\"name\":\"score\",\"type\":\"long\",\"doc\":\"User score\"},{\"name\":\"eventTime\",\"type\":\"long\",\"doc\":\"time when event created\"},{\"name\":\"eventTimeStr\",\"type\":\"string\",\"doc\":\"event time string for debugging\"}]}")
-  class Message
-
-  @BigQueryType.fromSchema("{\"type\":\"bigquerydefinition\",\"name\":\"BigQueryRow\",\"fields\":[{\"mode\":\"REQUIRED\",\"name\":\"user\",\"type\":\"STRING\"},{\"mode\":\"REQUIRED\",\"name\":\"teamName\",\"type\":\"STRING\"},{\"mode\":\"REQUIRED\",\"name\":\"score\",\"type\":\"INTEGER\"},{\"mode\":\"REQUIRED\",\"name\":\"eventTime\",\"type\":\"INTEGER\"},{\"mode\":\"REQUIRED\",\"name\":\"eventTimeStr\",\"type\":\"STRING\"},{\"mode\":\"REQUIRED\",\"name\":\"score2\",\"type\":\"FLOAT\"},{\"mode\":\"REQUIRED\",\"name\":\"processingTime\",\"type\":\"STRING\"}]}")
-  class BigQueryRow
-
   object conf {
     val jobConfig = SOTMacroJsonConfig(SchemaResourcePath().value)
     val sourceTaps = getSources(jobConfig)
     val sinkTaps = getSinks(jobConfig)
-    val sources = TapDef[parallelai.sot.executor.model.SOTMacroConfig.PubSubTapDefinition, parallelai.sot.engine.config.gcp.SOTUtils, com.spotify.scio.avro.types.AvroType.HasAvroAnnotation, Message](conf.sourceTaps(0)._3) :: HNil
-    val sinks = TapDef[parallelai.sot.executor.model.SOTMacroConfig.BigQueryTapDefinition, parallelai.sot.engine.config.gcp.SOTUtils, com.spotify.scio.bigquery.types.BigQueryType.HasAnnotation, BigQueryRow](conf.sinkTaps(0)._3) :: HNil
-  }
-
-  class Job extends Serializable {
-    def execute(sotUtils: SOTUtils, sc: ScioContext, args: Args): Unit = {
-      val job = init[HNil].flatMap(sColls => read(sc, TapDef[parallelai.sot.executor.model.SOTMacroConfig.PubSubTapDefinition, parallelai.sot.engine.config.gcp.SOTUtils, com.spotify.scio.avro.types.AvroType.HasAvroAnnotation, Message](conf.sourceTaps(0)._3), sotUtils)).flatMap(sColls => filter(sColls.at(Nat0()))(m => m.get('score) > 2)).flatMap(sColls => map(sColls.at(Nat1()))(m => m.append('score2, m.get('score) * 0.23d))).flatMap(sColls => map(sColls.at(Nat2()))(m => m.append('processingTime, Helper.fmt.print(Helper.Instant.now())))).flatMap(sColls => write(sColls.at(Nat3()))(TapDef[parallelai.sot.executor.model.SOTMacroConfig.BigQueryTapDefinition, parallelai.sot.engine.config.gcp.SOTUtils, com.spotify.scio.bigquery.types.BigQueryType.HasAnnotation, BigQueryRow](conf.sinkTaps(0)._3), sotUtils))
-      job.run(HNil)._1
-      val result = sc.close()
-      if (args.getOrElse("waitToFinish", "true").toBoolean) sotUtils.waitToFinish(result.internal)
-    }
   }
 
   def main(cmdArg: Array[String]): Unit = {
     val (sotOptions, sotArgs) = executionContext(cmdArg)
-    execute(new Job(), sotOptions, sotArgs)
+    execute(new Job, sotOptions, sotArgs)
   }
 
-  def executionContext(cmdArg: Array[String]): (SOTOptions, Args) = ScioContext.parseArguments[SOTOptions](cmdArg)
+  def executionContext(cmdArg: Array[String]): (SOTOptions, Args) =
+    ScioContext.parseArguments[SOTOptions](cmdArg)
 
   def execute[P <: PipelineOptions](job: Job, pipelineOptions: P, args: Args): Unit = {
     val sotUtils = new SOTUtils(pipelineOptions)
     val sc = ScioContext(pipelineOptions)
+
     job.execute(sotUtils, sc, args)
   }
 }
